abee38b047d7f118476b226173d4cf0a3ec8ecf9
Ning, Tao <ningtaous@gmail.com>
2016-02-15 17:04:20 -0500

    Implement tester/runner

---
+++ src/ObserverDesignPattern/WeatherStation.java
@@ -0,0 +1,13 @@
+package ObserverDesignPattern;
+
+public class WeatherStation {
+	public static void main(String[] args) {
+		WeatherData weatherData = new WeatherData();
+		CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
+		StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+		ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+		
+		weatherData.setMeasurements(80, 65, 30.4f);
+		weatherData.setMeasurements(75, 45, 41.2f);
+	}
+}
