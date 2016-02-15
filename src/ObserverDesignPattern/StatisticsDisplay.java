63d907974543ec49a7bfcca55f6869709e7fca2e
Ning, Tao <ningtaous@gmail.com>
2016-02-15 17:04:02 -0500

    Implement observer

---
+++ src/ObserverDesignPattern/StatisticsDisplay.java
@@ -0,0 +1,25 @@
+package ObserverDesignPattern;
+
+public class StatisticsDisplay implements Observer, DisplayElement{
+
+	private float temperature;
+	private float humidity;
+	private Subject weatherData;
+
+	public StatisticsDisplay(Subject weatherData) {
+		this.weatherData = weatherData;
+		weatherData.registerObserver(this);
+	}
+
+	public void update(float temperature, float humidity, float pressure) {
+		this.temperature = temperature;
+		this.humidity = humidity;
+		display();
+	}
+	
+	public void display() {
+		System.out.println("Statistics: " + temperature
+				+ "F degress and " + humidity + "% humidity");
+	}
+
+}
